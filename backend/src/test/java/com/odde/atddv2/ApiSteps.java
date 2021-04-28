3e076f7cbba4769aaa6f00bf83a989d73607b78e
leeonky <leeonky@gmail.com>
2021-04-28 12:33:01 +0000

    refactor mock server steps

--- backend/src/test/java/com/odde/atddv2/ApiSteps.java
+++ backend/src/test/java/com/odde/atddv2/ApiSteps.java
@@ -5,35 +5,26 @@ import com.odde.atddv2.entity.Order;
 import com.odde.atddv2.entity.OrderLine;
 import com.odde.atddv2.repo.OrderRepo;
 import io.cucumber.datatable.DataTable;
-import io.cucumber.java.Before;
 import io.cucumber.java.zh_cn.并且;
 import io.cucumber.java.zh_cn.当;
 import io.cucumber.java.zh_cn.那么;
-import lombok.SneakyThrows;
-import org.mockserver.client.MockServerClient;
-import org.mockserver.matchers.Times;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.transaction.Transactional;
-import java.net.URL;
-
-import static org.mockserver.model.HttpRequest.request;
-import static org.mockserver.model.HttpResponse.response;
-import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
 public class ApiSteps {
+    @Autowired
+    private MockServer mockServer;
+
     @Autowired
     private Api api;
 
     @Autowired
     private OrderRepo orderRepo;
 
-    private MockServerClient mockServerClient = createMockServerClient();
-
-    @当("API查询订单时")
-    public void api查询订单时() {
-        api.get("orders");
-    }
+    @Value("${binstd-endpoint.key}")
+    private String binstdAppKey;
 
     @那么("返回如下订单")
     public void 返回如下订单(String json) {
@@ -54,34 +45,15 @@ public class ApiSteps {
         api.get(String.format("orders/%s", code));
     }
 
-    @Before(order = 0)
-    public void resetMockServer() {
-        mockServerClient.reset();
-    }
-
     @并且("存在快递单{string}的物流信息如下")
     public void 存在快递单的物流信息如下(String deliverNo, String json) {
-        mockServerClient.when(
-                request().withMethod("GET")
-                        .withQueryStringParameter("appkey", "test")
-                        .withQueryStringParameter("type", "auto")
-                        .withQueryStringParameter("number", deliverNo)
-                        .withPath("/express/query"),
-                Times.unlimited()
-        ).respond(response()
-                .withStatusCode(200)
-                .withHeader(CONTENT_TYPE, "application/json")
-                .withBody(json));
+        mockServer.getJson("/express/query", (request) -> request.withQueryStringParameter("appkey", binstdAppKey)
+                .withQueryStringParameter("type", "auto")
+                .withQueryStringParameter("number", deliverNo), json);
     }
 
-    @SneakyThrows
-    private MockServerClient createMockServerClient() {
-        URL url = new URL("http://mock-server.tool.net:9081");
-        return new MockServerClient(url.getHost(), url.getPort()) {
-
-            @Override
-            public void close() {
-            }
-        };
+    @当("API查询订单时")
+    public void api查询订单时() {
+        api.get("orders");
     }
 }
